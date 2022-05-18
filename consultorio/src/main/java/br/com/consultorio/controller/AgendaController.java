b62a10ecc64d2e27854b3f5eab05eabae27da2a6
Layal Youssef Safa Al Wanni <nissolelo03@gmail.com>
2022-05-17 21:06:25 -0300

    EspecialidadeController feito

---
+++ consultorio/src/main/java/br/com/consultorio/controller/AgendaController.java
@@ -0,0 +1,4 @@
+package br.com.consultorio.controller;
+
+public class AgendaController {
+}
---
+++ consultorio/src/main/java/br/com/consultorio/controller/EspecialidadeController.java
@@ -0,0 +1,69 @@
+package br.com.consultorio.controller;
+
+import br.com.consultorio.entity.Especialidade;
+import br.com.consultorio.service.EspecialidadeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/api/especialidades")
+public class EspecialidadeController {
+
+    @Autowired
+    private EspecialidadeService especialidadeService;
+
+    @GetMapping("/{idEspecialidade}")
+    public ResponseEntity<Especialidade> findById(
+            @PathVariable("idEspecialidade") Long idEspecialidade
+
+    ){
+        return  ResponseEntity.ok().body(
+                this.especialidadeService.findById(idEspecialidade).get());
+    }
+
+
+    @GetMapping
+    public ResponseEntity<Page<Especialidade>> listByAllPages(Pageable pageable){
+        return ResponseEntity.ok().body(this.especialidadeService.listAll(pageable));
+    }
+
+
+    @PostMapping
+    public ResponseEntity<?> insert(@RequestBody Especialidade especialidade) {
+        try {
+            this.especialidadeService.insert(especialidade);
+            return ResponseEntity.ok().body("Especialidade Cadastrada com Sucesso");
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/{idEspecialidade}")
+    public ResponseEntity<?> update(
+            @RequestBody Especialidade especialidade,
+            @PathVariable Long idEspecialidade) {
+        try {
+            this.especialidadeService.update(idEspecialidade ,especialidade);
+            return ResponseEntity.ok().body("Especialidade Atualizada com Sucesso");
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/status/{idEspecialidade}")
+    public ResponseEntity<?> updateStatus(
+            @RequestBody Especialidade especialidade,
+            @PathVariable Long idEspecialidade) {
+        try {
+            this.especialidadeService.updateDataExcluido(idEspecialidade ,especialidade);
+            return ResponseEntity.ok().body("Especialidade Desativada com Sucesso");
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+}
